90db89ac19c6861b01b194ae14d8a0898f3509bd
Julio Antloga <julio.antloga@mindsight.com.br>
2025-10-16 16:13:58 -0300

    first db call

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def job(uuid):
     
     tenant_id, job_code = uuid.split("-")
     job = get_application(tenant_id,job_code)
-    return jsonify({"job_title": job["title"]})
+    return jsonify({"job_title": job[0]})
 
 
 # POST: recebe JSON e retorna processado

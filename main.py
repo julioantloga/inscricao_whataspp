4e3130f267d1b31c5985503899d529c12be5d158
Julio Antloga <julio.antloga@mindsight.com.br>
2025-10-23 17:12:04 -0300

    finalizar 5

--- main.py
+++ main.py
@@ -11,12 +11,24 @@ def home():
 
 # POST: recebe JSON e retorna processado
 @app.route("/add_application", methods=["POST"])
-def processar():
-    data = request.get_json()  # pega o JSON enviado no body
-    if not data:
-        return jsonify({"erro": "JSON inválido"}), 400
-    
-    return set_application (data)
+def add_application():
+    try:
+        data = request.get_json(force=True)
+        print("JSON recebido:", data)
+
+        context = data.get("context")
+        print("Context:", context)
+
+        # Verifica campos esperados
+        if not all(k in data for k in ["context", "phone", "tenant_name", "job_posting_id"]):
+            return jsonify({"erro": "Campos obrigatórios faltando"}), 400
+
+        return jsonify({"mensagem": "Recebido com sucesso!", "dados": data})
+
+    except Exception as e:
+        print("Erro interno:", e)
+        return jsonify({"erro": str(e)}), 500
+    set_application (data)
 
 # Executa localmente (Railway ignora essa parte no deploy)
 if __name__ == "__main__":
